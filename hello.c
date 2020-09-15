d324f21a3c7a14abaff26ee93f82188b8e21255f
zhiwen <lzw5399@gmail.com>
2020-09-16 00:31:20 +0800

    feat: retro c

---
+++ hello.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    // 整形
+    printf("int占用字节数, %llu \n", sizeof(int)); // 根据计算机的操作系统，有可能是short或者long
+    printf("unsigned int占用字节数, %llu \n", sizeof(unsigned int));// 根据计算机的操作系统，有可能是unsigned short或者unsigned long
+    printf("short占用字节数, %llu \n", sizeof(short));
+    printf("long占用字节数, %llu \n", sizeof(long));
+
+    // 浮点型
+    printf("float占用字节数, %llu \n", sizeof(float));
+    printf("double占用字节数, %llu \n", sizeof(double));
+    printf("long double占用字节数, %llu \n", sizeof(long double));
+
+    // 布尔
+    _Bool b1 = 1;
+    int bo = 1;
+    bo = "ewew";
+    if (bo && b1) {
+        printf("。。。\n");
+    }
+
+    bo = "0";
+    if (bo){
+        printf("这尼玛也行？\n");
+    }
+
+    return 0;
+}
+
+enum Gender {
+    Male,
+    Female
+};
